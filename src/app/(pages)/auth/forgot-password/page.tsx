1092864ccef734d234b6ef1d23f547ff54796877
giojano1 <giorgi.janelidze64@gmail.com>
2025-12-10 12:39:55 +0400

    bugFix/build-errors

--- src/app/(pages)/auth/forgot-password/page.tsx
+++ src/app/(pages)/auth/forgot-password/page.tsx
@@ -1,5 +1,5 @@
 import AuthPageWrapper from "@/features/auth/shared/components/page-wrapper";
-import ForgotPasswordForm from "@/features/auth/password/forgot/forgot-password-form";
+import ForgotPasswordForm from "@/features/auth/password/forgot/forgot-password.form";
 import { authRoutes } from "@/constants/routes";
 import { Metadata } from "next";
 
