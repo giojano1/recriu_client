9c589de3479216d9d0b184319b5ebf5891a8827f
giojano1 <giorgi.janelidze64@gmail.com>
2025-12-11 00:14:50 +0400

    feat/copy-public-url

--- src/components/layout/dashboard/header/public-page-btn.tsx
+++ src/components/layout/dashboard/header/public-page-btn.tsx
@@ -1,11 +1,31 @@
+"use client";
+
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Copy } from "lucide-react";
+import { toast } from "sonner";
 
 export default function PublicPageBtn({ className }: { className?: string }) {
+  const [disabled, setDisabled] = useState(false);
+
+  const handleCopy = async () => {
+    if (disabled) return;
+
+    setDisabled(true);
+
+    const url = "https://recriu.com/app_name";
+    await navigator.clipboard.writeText(url);
+
+    setTimeout(() => setDisabled(false), 2000);
+    toast.success("Public page URL copied to clipboard!");
+  };
+
   return (
     <Button
       size="sm"
       variant="outline"
+      disabled={disabled}
+      onClick={handleCopy}
       className={`flex items-center gap-2 ${className}`}
     >
       <div className="mt-0.5 -mr-0.5 size-2 rounded-full bg-green-500" />
