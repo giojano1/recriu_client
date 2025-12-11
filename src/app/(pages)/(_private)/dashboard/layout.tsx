0b43efd2d7d09a47b1ede24c85063dcc2d48c65e
giojano1 <giorgi.janelidze64@gmail.com>
2025-12-12 01:23:20 +0400

    bugFix/overscroll-y-none

--- src/app/(pages)/(_private)/dashboard/layout.tsx
+++ src/app/(pages)/(_private)/dashboard/layout.tsx
@@ -10,7 +10,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
       <DashboardSidebar />
       <SidebarInset>
         <Header />
-        <div className="flex flex-1 flex-col gap-4 overflow-y-auto px-4">
+        <div className="flex flex-1 flex-col gap-4 overflow-y-auto">
           {children}
         </div>
       </SidebarInset>
